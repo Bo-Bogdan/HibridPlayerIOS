e81cfc278f8cd9834807c6c37000132788f81b6b
Bogdan Tykhiy <>
2021-07-09 02:21:42 +0300

    commit project

---
+++ HibridPlayerIOS/HibridPlayerIOS.swift
@@ -0,0 +1,16 @@
+//
+//  HibridPlayerIOS.swift
+//  HibridPlayerIOS
+//
+//  Created by Bogdan T on 08.07.2021.
+//
+
+import Foundation
+import HibridStreamPlayer
+
+public class HibridPlayerIOS: NSObject {
+    
+    var HibridPlayerView: HibridPlayerView!
+    
+    
+}
